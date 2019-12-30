923a35963d3e02685f97405cacff0baecd5c1be0
Lahchhsin <nishchhalbhardwaj94@gmail.com>
2019-12-30 17:15:23 +0530

    modified index,style and server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(express.static("public"));
+// const PORT = process.env.PORT || 5000;
+app.use(express.static("docs"));
 app.use(express.json());
 
 app.get("/getdata", (res,req,err)=>{
@@ -11,7 +11,7 @@ app.get("/getdata", (res,req,err)=>{
 })
 
 
-app.listen(PORT,()=>{
+app.listen(5000,()=>{
 
     console.log("runningn at 5000");
 
